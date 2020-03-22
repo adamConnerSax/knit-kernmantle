fc08622b7e5316e238d1491a4002996acf2b7206
adamConnerSax <adam_conner_sax@yahoo.com>
2020-03-22 16:08:31 -0400

    Getting somewhere...

--- src/Knit/Kernmantle/Effect/Core.hs
+++ src/Knit/Kernmantle/Effect/Core.hs
@@ -1,5 +1,8 @@
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE OverloadedLabels #-}
+{-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeOperators       #-}
 module Knit.Kernmantle.Effect.Core
@@ -9,6 +12,8 @@ module Knit.Kernmantle.Effect.Core
   ) where
 
 import qualified Control.Kernmantle.Rope as Rope
+import           Control.Kernmantle.Rope ((&))
+import           Data.Profunctor.Trans (type (~>))
 import qualified Polysemy as P
 import qualified Knit.Report.EffectStack as K
 import qualified Knit.Effect.Logger as K
@@ -22,20 +27,41 @@ import qualified Data.Text as T
 -- these effects directly in Kernmantle as well,
 -- eventually leaving the base as just the PandocMonad
 -- and IO bits, hopefully.
+-- Basically, leave the truly monadic bits in the core
+-- and move anything fundamentally applicative to
+-- the arrow/kernmantle layer.
 type KnitMonad m = P.Sem (K.KnitEffectStack m)
-type KnitKleisli m a b = A.Kleisli (KnitMonad m) a b
+type KnitKleisli m = A.Kleisli (KnitMonad m) 
 
-arrInSemCore :: (a -> KnitMonad m b) -> KnitKleisli m a b
-arrInSemCore = A.Kleisli 
+-- this will evolve as we reinterpret and manage effects above the monadic Knit core
+type KnitCore m = KnitKleisli m 
 
-runInSemCore :: KnitMonad m a -> KnitKleisli m () a
-runInSemCore = arrInSemCore . const 
+arrKnitMonad :: (a -> KnitMonad m b) -> KnitCore m a b
+arrKnitMonad = A.Kleisli 
 
+runInKnitMonad :: KnitMonad m a -> KnitCore m () a
+runInKnitMonad = arrKnitMonad . const 
+
+-- run
+type KnitPipeline m a b = Rope.AnyRopeWith '[ '("log", LogEff), '("knitCore", KnitCore m)] '[] a b
+
+runKnitPipeline :: KnitPipeline m a b -> a -> KnitMonad m b
+runKnitPipeline pipeline input = pipeline
+                                 & Rope.loosen
+                                 & Rope.weaveK #log runLogEffInKnitMonad
+                                 & Rope.weave' #knitCore id
+                                 & Rope.untwine
+                                 & (flip A.runKleisli input)
+                                 
+                                 
 
 -- The Logging Effect
 data LogEff a b where
   LogText :: LogEff (K.LogSeverity, T.Text) ()
-  
-runLogEff :: LogEff a b -> KnitKleisli m a b
-runLogEff LogText = A.Kleisli $ uncurry K.logLE 
+
+runLogEffInKnitMonad :: a `LogEff` b -> a -> KnitMonad m b
+runLogEffInKnitMonad LogText = uncurry K.logLE
+
+interpLogEff :: a `LogEff` b -> Rope.AnyRopeWith '[ '("knitCore", KnitCore m)] '[] a b 
+interpLogEff  = Rope.strand #knitCore . A.Kleisli . runLogEffInKnitMonad
 
